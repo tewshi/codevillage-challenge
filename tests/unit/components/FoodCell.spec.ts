3c5ccd3783a88c5596dbc01d4a8f177ff70206d8
Joseph Ojoko <entewshi@gmail.com>
2021-04-15 04:41:55 +0100

    wrap up tests

--- tests/unit/components/FoodCell.spec.ts
+++ tests/unit/components/FoodCell.spec.ts
@@ -16,4 +16,15 @@ describe("FoodCell.vue", () => {
     });
     expect(wrapper.props("col")).toEqual(col);
   });
+
+  it("renders contains data", async () => {
+    const wrapper = shallowMount(FoodCell, {
+      props: { col },
+    });
+    expect(wrapper.vm.value).toEqual(col);
+    expect(wrapper.vm.untouched).toBeTruthy();
+    expect(wrapper.vm.hasFood).toBeFalsy();
+    expect(wrapper.vm.isEmpty).toBeFalsy();
+    expect(wrapper.vm.isPlayer).toBeFalsy();
+  });
 });
